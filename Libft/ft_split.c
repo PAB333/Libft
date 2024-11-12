8d5dda8b501c2df86a21b030863db1c40c2d1f71
Pierre-Antoine Breissand <pibreiss@z3r11p6.42lyon.fr>
2024-11-12 11:23:49 +0100

    split

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: pibreiss <pibreiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:54:51 by pibreiss          #+#    #+#             */
-/*   Updated: 2024/11/11 18:48:57 by pibreiss         ###   ########.fr       */
+/*   Updated: 2024/11/12 11:21:41 by pibreiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,65 @@
 
 static size_t	count_string(char const *s, char c)
 {
-	size_t	i;
 	size_t	count;
-	size_t	string;
+	size_t	i;
 
-	i = -1;
 	count = 0;
-	string = 0;
-	while (s[++i])
+	i = 0;
+	while (s[i])
 	{
-		if (s[i] != c && string == 0)
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i] && s[i] != c)
 		{
-			string = 1;
 			count++;
+			while (s[i] && s[i] != c)
+				i++;
 		}
-		else if (s[i] == c)
-			string = 0;
-		i++;
 	}
 	return (count);
+}
+
+static char	*make_array(size_t start, size_t end, const char *s)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		str[i++] = s[start++];
+	str[i] = '\0';
+	return (str);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**array;
+	size_t	start;
+	size_t	end;
+	size_t	i;
+
+	start = 0;
+	end = 0;
+	i = 0;
+	array = (char **)malloc(sizeof(char *) * (count_string(s, c) + 1));
+	if (array == NULL)
+		return (NULL);
+	while (s[end])
+	{
+		if (s[end] == c)
+		{
+			if (end > start)
+				array[i++] = make_array(start, end, s);
+			start = end + 1;
+		}
+		end++;
+	}
+	if (end > start)
+		array[i++] = make_array(start, end, s);
+	array[i] = NULL;
+	return (array);
 }
