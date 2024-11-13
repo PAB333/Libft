c9fbad1b372aa773bd32d88b140572bca891ebf2
PAB333 <pabrd3@gmail.com>
2024-11-13 19:28:28 +0100

    ft_lstadd_back

--- Libft/_bonus/ft_lstadd_back.c
+++ Libft/_bonus/ft_lstadd_back.c
@@ -1,29 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_bonus.h                                      :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pibreiss <pibreiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/13 15:54:47 by pibreiss          #+#    #+#             */
-/*   Updated: 2024/11/13 19:12:48 by pibreiss         ###   ########.fr       */
+/*   Created: 2024/11/13 19:11:54 by pibreiss          #+#    #+#             */
+/*   Updated: 2024/11/13 19:25:46 by pibreiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_BONUS_H
-# define LIBFT_BONUS_H
+#include "libft_bonus.h"
 
-# include <stdlib.h>
-
-typedef struct s_list
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	void			*content;
-	struct s_list	*next;
-}					t_list;
+	t_list	*current;
 
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-#endif
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		current = *lst;
+		current = ft_lstlast(current);
+		current->next = new;
+	}
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: pibreiss <pibreiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:19:44 by pibreiss          #+#    #+#             */
-/*   Updated: 2024/11/13 18:59:33 by pibreiss         ###   ########.fr       */
+/*   Updated: 2024/11/13 19:12:57 by pibreiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,6 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
