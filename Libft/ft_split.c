d1c8ae09147eefb46735c2e67dc47e9c693a64c7
PAB333 <pabrd3@gmail.com>
2024-11-14 16:34:24 +0100

    split not finish

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,13 +6,13 @@
 /*   By: pibreiss <pibreiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:54:51 by pibreiss          #+#    #+#             */
-/*   Updated: 2024/11/13 18:38:46 by pibreiss         ###   ########.fr       */
+/*   Updated: 2024/11/14 16:33:45 by pibreiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	count_string(char const *s, char c)
+static size_t	count_words(char const *s, char c)
 {
 	size_t	count;
 	size_t	i;
@@ -48,6 +48,16 @@ static char	*make_array(size_t start, size_t end, const char *s)
 	return (str);
 }
 
+static void	free_array(char **array, size_t size)
+{
+	size_t	i;
+
+	i = -1;
+	while (++i < size)
+		free(array[i]);
+	free(array);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**array;
@@ -56,23 +66,38 @@ char	**ft_split(char const *s, char c)
 	size_t	i;
 
 	start = 0;
-	end = 0;
+	end = -1;
 	i = 0;
-	array = (char **)malloc(sizeof(char *) * (count_string(s, c) + 1));
+	array = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1));
 	if (array == NULL)
 		return (NULL);
-	while (s[end])
+	while (s[++end])
 	{
 		if (s[end] == c)
 		{
 			if (end > start)
-				array[i++] = make_array(start, end, s);
+			{
+				array[i] = make_array(start, end, s);
+				if (array[i] == NULL)
+				{
+					free_array(array, i);
+					return (NULL);
+				}
+				i++;
+			}
 			start = end + 1;
 		}
-		end++;
 	}
 	if (end > start)
-		array[i++] = make_array(start, end, s);
+	{
+		array[i] = make_array(start, end, s);
+		if (array[i] == NULL)
+		{
+			free_array(array, i);
+			return (NULL);
+		}
+		i++;
+	}
 	array[i] = NULL;
 	return (array);
 }
